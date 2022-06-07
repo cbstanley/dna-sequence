5aa991f7f8227aee884560d063a5fe856f93ba88
Chris Stanley <cbstanley@users.noreply.github.com>
2022-06-06 22:55:25 -0400

    cleanup

--- explore_seqIO.py
+++ explore_seqIO.py
@@ -9,6 +9,8 @@ import numpy as np
 import re
 
 from Bio import SeqIO
+from Bio import pairwise2
+from Bio.pairwise2 import format_alignment
 
 
 # for record in SeqIO.parse('fasta/NC_000011.10.fasta', "fasta"):
@@ -18,12 +20,15 @@ from Bio import SeqIO
 #     # seq_id = record.id
 #     # seq = record.seq
 
-record = SeqIO.read('fasta/NC_000011.10.fasta', "fasta")
-print(record.id)
-print(len(record))
+# get 2 test sequences
+seq1 = SeqIO.read('fasta/NC_000011.10.fasta', "fasta")
+seq2 = SeqIO.read('fasta/NC_000012.12.fasta', "fasta")
+
+print(seq1.id)
+print(len(seq1))
 
 # get seq as a string and convert to lower
-one_seq = str(record.seq.lower())
+one_seq = str(seq1.seq.lower())
 # print(one_seq[:10])
 
 # convert to array
@@ -31,15 +36,11 @@ seq_arr = np.array(list(one_seq))
 print(seq_arr[:10])
 
 
-# ----------------
-
-seq1 = SeqIO.read('fasta/NC_000011.10.fasta', "fasta")
-seq2 = SeqIO.read('fasta/NC_000012.12.fasta', "fasta")
-
-from Bio import pairwise2
-from Bio.pairwise2 import format_alignment
-
+# --------------------
+# calculate alignments
 alignments = pairwise2.align.globalxx(seq1, seq2)
-for alignment in alignments:
-    print(alignment)
-    print(format_alignment(*alignment))
+print(alignments[0])
+
+# for alignment in alignments:
+#     print(alignment)
+#     print(format_alignment(*alignment))
